33bcada289ff505bfb8d77391fd331add1d98d6f
Alejandro-Alvarez-Espinosa <alejandro.alvarez17u@std.uni.edu.ni>
2024-05-12 21:39:27 -0600

    Se creo el proyecto

---
+++ SimulacionVehiculo/Automovil_2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulacionVehiculo
+{
+    public partial class Automovil
+    {
+        public bool Conducir(int distancia)
+        {
+          double CombustibleNecesario = distancia / RendimientoCombustible;
+
+            if( NivelCombustible >= CombustibleNecesario)
+            {
+                NivelCombustible -= CombustibleNecesario;
+                Console.WriteLine($"El vehiculo {Marca} {Modelo} pudo recorrer {distancia} KM");
+            }
+            else
+            {
+                Console.WriteLine($"El vehiculo {Marca} {Modelo} no tiene suficiente combustible para viajar {distancia} Km. Se requieren {CombustibleNecesario - NivelCombustible} galones extra");
+            }
+               return CombustibleNecesario <= NivelCombustible;
+        }
+
+        public void ReabastecerCombustible(double cantidad)
+        {
+            if (cantidad > 0)
+            {
+                double TanqueMedio = NivelCombustible + cantidad;
+                if (TanqueMedio > CapacidadTanqueCombustible)
+                {
+                    NivelCombustible = CapacidadTanqueCombustible;
+                    Console.WriteLine($"Se ha reabastecido {CapacidadTanqueCombustible - NivelCombustible} galones, tanque lleno ");
+                }
+                else
+                {
+                    NivelCombustible = TanqueMedio;
+                    Console.WriteLine($"Se he reabastecido {cantidad} galones");
+                }
+            }
+            else
+            {
+                Console.WriteLine("La cantidad para llenar de ser mayor que 0 ");
+            }
+        }
+
+        public bool PuedeConducir(int distancia)
+        {
+            double CombustibleNecesario = distancia / RendimientoCombustible;
+
+            if(NivelCombustible >= CombustibleNecesario)
+            {
+                return true; //Si hay Gasolina para ir a Pochomil
+            }
+            else
+            {
+                return false; //No hay Gasolina para ir a Pochomil
+            }
+        }
+    }
+}
---
+++ SimulacionVehiculo/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SimulacionVehiculo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******************************");
+            Console.WriteLine("Descripcion de Automoviles: ");
+            Console.WriteLine("******************************");
+
+            //Instancias de automovil
+            Automovil automovil1 = new Automovil("Lamborghini", "GDW - 571", "Huracan", "Dorado", 25, 30);
+            Console.WriteLine("----------------------------------------------------------------------------------------------------------------------");
+            Automovil automovil2 = new Automovil("Ferrari", "VGF - 786", "SF90 Deportivo", "Plateado", 20, 25);
+
+            //Acciones de conducir y llenar el tanque
+            automovil1.Conducir(180);//Se necesita llenar el tanque para viajar
+            Console.WriteLine("----------------------------------------------------------------------------------------------------------------------");
+            automovil2.Conducir(150);//Se necesita llenar el tanque para viajar
+            Console.WriteLine("----------------------------------------------------------------------------------------------------------------------");
+            automovil1.ReabastecerCombustible(4);
+            automovil1.Conducir(180);
+            Console.WriteLine("----------------------------------------------------------------------------------------------------------------------");
+            automovil2.ReabastecerCombustible(6);
+            automovil2.Conducir(120); //Ahora si puede viajar
+
+            Console.WriteLine("----------------------------------------------------------------------------------------------------------------------");
+            //Probar si el auto puede viajar una distancia especifica 
+            if (automovil1.PuedeConducir(200))
+            {
+                Console.WriteLine($"El automovil {automovil1.Marca} {automovil1.Modelo} puede viajar 200 KM ");
+            }
+            else
+            {
+                Console.WriteLine($"El vehículo {automovil1.Marca} {automovil1.Modelo} no tiene suficiente combustible para recorrer 200 km.");
+
+            }
+        }
+    }
+}
